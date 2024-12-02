156c9861320e067de4d52a9d13b250cc05479864
risky ramdani darius <125119919+dariusrisky@users.noreply.github.com>
2024-12-02 16:42:28 +0700

    Update conf.php

--- conf.php
+++ conf.php
@@ -8,22 +8,22 @@ $client->setApplicationName('Google Sheets API');
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
 
 $client->setAccessType('offline');
-$path = './googleapis.json';
+$path = ':PATH_JSON';
 $client->setAuthConfig($path);
 
 $service = new \Google_Service_Sheets($client);
 
-$spreadsheetId = '1m43T2EJavNtHMj7Y32JXNZA40WaPmqV0smfMKvl4NGE';
+$spreadsheetId = ':SPREADSHEET_ID';
 $spreadsheet = $service->spreadsheets->get($spreadsheetId);
 
-$range = 'Sheet2!A1:B4';
+$range = 'Sheet1';
 $res = $service->spreadsheets_values->get($spreadsheetId, $range);
 $value = $res->getValues();
 
 // var_dump($value);
 
 foreach ($value as $arr) {
-     echo $arr[0]. $arr[1]."<br>";
+     echo $arr[0]"<br>";
 }
 
 // echo(json_encode($value));
